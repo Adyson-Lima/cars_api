8637605c99f1bc8b57b05aae8e7f354d48edce2e
adyson <adyv8@yahoo.com.br>
2023-10-24 11:05:24 -0300

    12° commit - tdd controller: Consegue listar um car especifico e retornar status 200? RED

--- spec/controllers/v1/cars_controller_v1_spec.rb
+++ spec/controllers/v1/cars_controller_v1_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Api::V1::CarsController, type: :controller do
     end
   end
   
+  describe 'GET /api/v1/cars/id' do
+    it 'Consegue listar um car especifico e retornar status 200?' do
+      get :show, params: {id: @car.id}
+      expect(response.body).to include_json(id: @car.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+  
 end
